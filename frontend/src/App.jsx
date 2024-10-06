72c010eaedaa1e753e01a51648b5aaa949fc854b
Abdul Hanan Abbasi <146124897+123superhanan@users.noreply.github.com>
2024-10-06 14:16:50 +0500

    jokes fetching & proxy cors managment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([]);
 
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) =>{
+      setJokes(response.data);
+    })
+    .catch((error) =>{
+      console.error(" Error", error)
+    })
+  },[])
   return (
     <>
-      
-     
+    <div className='name'>
+      <h1>Jokes Fullstack App</h1>
+      <h2>JSON Fetching From backend</h2>
+      <div className='jokes'>
+      <p className='pp'>JOKES: {jokes.length}</p>
+
+      {
+         jokes.map((joke, index) =>( 
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.joke}</p>
+          </div>
+        ))
+      }
+      </div>
+      </div>
     </>
   )
 }
